17c8a435dc859b7a762867a734dbe193c444d77a
Rosario Scilipoti <rosariosca12@icloud.com>
2023-10-18 10:42:11 +0200

    finita pagina

--- script/questionario.js
+++ script/questionario.js
@@ -5,12 +5,8 @@ const questions = [
     difficulty: "easy",
     question: "What does CPU stand for?",
     correct_answer: "Central Processing Unit",
-    incorrect_answers: [
-      "Central Process Unit",
-      "Computer Personal Unit",
-      "Central Processor Unit"
-    ],
-    timelimit: 60
+    incorrect_answers: ["Central Process Unit", "Computer Personal Unit", "Central Processor Unit"],
+    timelimit: 60,
   },
   {
     category: "Science: Computers",
@@ -20,7 +16,7 @@ const questions = [
       "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?",
     correct_answer: "Final",
     incorrect_answers: ["Static", "Private", "Public"],
-    timelimit: 60
+    timelimit: 60,
   },
   {
     category: "Science: Computers",
@@ -29,27 +25,25 @@ const questions = [
     question: "The logo for Snapchat is a Bell.",
     correct_answer: "False",
     incorrect_answers: ["True"],
-    timelimit: 30
+    timelimit: 30,
   },
   {
     category: "Science: Computers",
     type: "boolean",
     difficulty: "easy",
-    question:
-      "Pointers were not used in the original C programming language; they were added later on in C++.",
+    question: "Pointers were not used in the original C programming language; they were added later on in C++.",
     correct_answer: "False",
     incorrect_answers: ["True"],
-    timelimit: 30
+    timelimit: 30,
   },
   {
     category: "Science: Computers",
     type: "multiple",
     difficulty: "easy",
-    question:
-      "What is the most preferred image format used for logos in the Wikimedia database?",
+    question: "What is the most preferred image format used for logos in the Wikimedia database?",
     correct_answer: ".svg",
     incorrect_answers: [".png", ".jpeg", ".gif"],
-    timelimit: 60
+    timelimit: 60,
   },
   {
     category: "Science: Computers",
@@ -57,22 +51,17 @@ const questions = [
     difficulty: "easy",
     question: "In web design, what does CSS stand for?",
     correct_answer: "Cascading Style Sheet",
-    incorrect_answers: [
-      "Counter Strike: Source",
-      "Corrective Style Sheet",
-      "Computer Style Sheet"
-    ],
-    timelimit: 60
+    incorrect_answers: ["Counter Strike: Source", "Corrective Style Sheet", "Computer Style Sheet"],
+    timelimit: 60,
   },
   {
     category: "Science: Computers",
     type: "multiple",
     difficulty: "easy",
-    question:
-      "What is the code name for the mobile operating system Android 7.0?",
+    question: "What is the code name for the mobile operating system Android 7.0?",
     correct_answer: "Nougat",
     incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"],
-    timelimit: 60
+    timelimit: 60,
   },
   {
     category: "Science: Computers",
@@ -81,7 +70,7 @@ const questions = [
     question: "On Twitter, what is the character limit for a Tweet?",
     correct_answer: "140",
     incorrect_answers: ["120", "160", "100"],
-    timelimit: 60
+    timelimit: 60,
   },
   {
     category: "Science: Computers",
@@ -90,22 +79,32 @@ const questions = [
     question: "Linux was first created as an alternative to Windows XP.",
     correct_answer: "False",
     incorrect_answers: ["True"],
-    timelimit: 30
+    timelimit: 30,
   },
   {
     category: "Science: Computers",
     type: "multiple",
     difficulty: "easy",
-    question:
-      "Which programming language shares its name with an island in Indonesia?",
+    question: "Which programming language shares its name with an island in Indonesia?",
     correct_answer: "Java",
     incorrect_answers: ["Python", "C", "Jakarta"],
-    timelimit: 60
-  }
+    timelimit: 60,
+  },
 ];
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+shuffleArray(questions);
+
 let currentQuestionIndex = 0;
 let score = 0;
 let timer;
+
 const questionContainer = document.getElementById("question-container");
 const resultContainer = document.getElementById("result-container");
 const timerElement = document.getElementById("timer");
@@ -117,12 +116,11 @@ function displayQuestion() {
     questionContainer.innerHTML = "";
     resultContainer.innerHTML = `Punteggio finale: ${score} su ${questions.length}`;
     timerElement.style.display = "none";
+    questionIndexElement.style.display = "none";
     return;
   }
 
-  questionIndexElement.textContent = `Domanda ${currentQuestionIndex + 1} di ${
-    questions.length
-  }`;
+  questionIndexElement.textContent = `Domanda ${currentQuestionIndex + 1} di ${questions.length}`;
 
   questionContainer.innerHTML = `<p>${question.question}</p>`;
 
@@ -139,10 +137,10 @@ function displayQuestion() {
   `;
 
   questionContainer.innerHTML += `
-    <button id="next-button" onclick="nextQuestion()" style="visibility: visible;">Domanda successiva</button>
+    <button id="next-button" onclick="nextQuestion()" style="visibility: hidden;">Domanda successiva</button>
   `;
 
-  startTimer(question.type);
+  startTimer(question.timelimit);
 
   const answerOptions = document.querySelectorAll('input[name="answer"]');
   answerOptions.forEach((option) => {
@@ -152,13 +150,8 @@ function displayQuestion() {
   checkSelectedAnswer();
 }
 
-function startTimer(questionType) {
-  let timeRemaining;
-  if (questionType === "multiple") {
-    timeRemaining = 60;
-  } else if (questionType === "boolean") {
-    timeRemaining = 30;
-  }
+function startTimer(timeLimit) {
+  let timeRemaining = timeLimit;
 
   timerElement.style.display = "inline";
   updateTimerDisplay(timeRemaining);
@@ -180,20 +173,17 @@ function updateTimerDisplay(timeRemaining) {
 
 function nextQuestion() {
   const selectedAnswer = document.querySelector('input[name="answer"]:checked');
-  if (
-    selectedAnswer &&
-    selectedAnswer.value === questions[currentQuestionIndex].correct_answer
-  ) {
+  if (selectedAnswer && selectedAnswer.value === questions[currentQuestionIndex].correct_answer) {
     score++;
   }
 
   currentQuestionIndex++;
   clearInterval(timer);
   if (currentQuestionIndex >= questions.length) {
-    // Il quiz è finito, visualizza il risultato finale
     questionContainer.innerHTML = "";
     resultContainer.innerHTML = `Punteggio finale: ${score} su ${questions.length}`;
     timerElement.style.display = "none";
+    questionIndexElement.style.display = "none";
   } else {
     displayQuestion();
   }
@@ -215,6 +205,16 @@ function checkSelectedAnswer() {
 }
 
 function handleAnswerSelection() {
+  const answerLabels = document.querySelectorAll("label");
+  answerLabels.forEach((label) => {
+    label.classList.remove("selected");
+  });
+
+  const selectedLabel = document.querySelector('input[name="answer"]:checked + label');
+  if (selectedLabel) {
+    selectedLabel.classList.add("selected");
+  }
+
   checkSelectedAnswer();
 }
 
